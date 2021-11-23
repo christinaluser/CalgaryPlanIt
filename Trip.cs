a7e5bd69f2f3ae2cd5bcbae683e6f789fe5c20c5
Christina Lu <christinalu42@gmail.com>
2021-11-22 20:04:30 -0700

    added fonts and some classes

---
+++ Trip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalgaryPlanIt
+{
+    public class Trip
+    {
+        public List<ItineraryItem> ItineraryItems { get; set; }
+    }
+}
