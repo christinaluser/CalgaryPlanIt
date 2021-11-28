e9dd17ee3a8ea8a8914973bceb5dfce1890b24b6
mojanjua1 <muhammad.janjua@ucalgary.ca>
2021-11-28 14:42:44 -0700

    Added first list page, still have to add button to interact with 2nd list page, also styling needs work

---
+++ CalgaryPlanIt/Components/ListCard.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalgaryPlanIt.Components
+{
+    /// <summary>
+    /// Interaction logic for ListCard.xaml
+    /// </summary>
+    public partial class ListCard : UserControl
+    {
+        public Lis lis;
+        public ListCard()
+        {
+            InitializeComponent();
+            this.lis = new Lis();
+        }
+
+        public ListCard(Lis lis)
+        {
+            InitializeComponent();
+            this.lis = lis;
+            ListName.Content = this.lis.Name;
+            if(lis.NumItems > 0)
+            {
+                NumItems.Content = lis.NumItems.ToString()+" items";
+            }
+        }
+    }
+}
