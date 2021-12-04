0c1535099d62138c0d6237b3c3de3b84fe4c0042
Christina Lu <christinalu42@gmail.com>
2021-12-04 07:48:45 -0700

    Style sort and search

--- CalgaryPlanIt/Views/Lists.xaml.cs
+++ CalgaryPlanIt/Views/Lists.xaml.cs
@@ -21,21 +21,66 @@ namespace CalgaryPlanIt.Views
     /// </summary>
     public partial class Lists : Page
     {
-        
+        string SortType = "A-Z";
+
+
         public Lists()
         {
             InitializeComponent();
-            AddListCard();
+            AddListCards(MainWindow.ListofLists);
         }
 
-        private void AddListCard()
+        private void AddListCards(List<Lis> ListofLists)
         {
             ListGrid.Children.Clear();
-            foreach(Lis lis in MainWindow.ListofLists)
+            foreach(Lis lis in ListofLists)
             {
                 ListGrid.Children.Add(new ListCard(lis));
             }
 
+        }
+
+        private void Sort()
+        {
+            if (SortType == "A-Z")
+            {
+                MainWindow.ListofLists = MainWindow.ListofLists.OrderBy(t => t.Name).ToList();
+            }
+            else if (SortType == "Z-A")
+            {
+                MainWindow.ListofLists = MainWindow.ListofLists.OrderByDescending(t => t.Name).ToList();
+            }
+            
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var split = ((ComboBox)sender).SelectedItem.ToString().Split(" ");
+            SortType = split[1];
+            if (split.Length > 2)
+                SortType += " " + split[2];
+            Sort();
+            if (ListGrid != null && ListGrid.Children.Count > 0)
+                AddListCards(MainWindow.ListofLists);
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                var searchVal = SearchBox.Text;
+                List<Lis> searchResults = MainWindow.ListofLists.FindAll(t => t.Name.Contains(searchVal));
+                SearchResultsTitle.Text += "\"" + searchVal + "\"    (" + searchResults.Count + " results)";
+                SearchHeader.Visibility = Visibility.Visible;
+                AddListCards(searchResults);
+            }
+        }
+
+        private void ClearSearchResults(object sender, EventArgs e)
+        {
+            SearchBox.Clear();
+            SearchHeader.Visibility = Visibility.Collapsed;
+            AddListCards(MainWindow.ListofLists);
         }
     }
 }
