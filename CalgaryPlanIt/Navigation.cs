d9b04a04be2874eccd54a4a14e6653cdfc34759f
Christina Lu <christinalu42@gmail.com>
2021-12-06 21:35:03 -0700

    navbar

--- CalgaryPlanIt/Navigation.cs
+++ CalgaryPlanIt/Navigation.cs
@@ -13,12 +13,25 @@ namespace CalgaryPlanIt
 
         public static void NavigateTo(Page newPage)
         {
+            window.navbar.Visibility = System.Windows.Visibility.Visible;
+            window.mobilenavbar.Visibility = System.Windows.Visibility.Collapsed;
+            window.Width = 1500;
             window.SwitchPage(newPage);
         }
         
         public static void HightLightFromStartPlanning ()
         {
             window.navbar.HighlightNavBarButton("Things To Do");
+        }
+
+        public static void NavigateToMobile(Page newPage)
+        {
+            window.navbar.Visibility = System.Windows.Visibility.Collapsed;
+            window.mobilenavbar.Visibility = System.Windows.Visibility.Visible;
+            window.Width = 450;
+
+            window.SwitchPage(newPage);
+            window.mobilenavbar.NavBarButtonGroup.Visibility = System.Windows.Visibility.Collapsed;
         }
     }
 }
